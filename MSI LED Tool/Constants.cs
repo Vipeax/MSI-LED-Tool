a072db51989cbdc90861490110b745418601d7e7
Robert van den Boorn <robert.vd.boorn@gmail.com>
2018-06-14 20:54:00 +0200

    Added GTX 1070Ti

--- MSI LED Tool/Constants.cs
+++ MSI LED Tool/Constants.cs
@@ -14,6 +14,7 @@ namespace MSI_LED_Tool
         #region Pascal
         public const string DeviceCodeGtx1080Ti = "1B06";
         public const string DeviceCodeGtx1080 = "1B80";
+        public const string DeviceCodeGtx1070Ti = "1B82";
         public const string DeviceCodeGtx1070 = "1B81";
         public const string DeviceCodeGtx1060With6G = "1C03";
         public const string DeviceCodeGtx1060With3G = "1C02";
@@ -36,6 +37,7 @@ namespace MSI_LED_Tool
         {
             DeviceCodeGtx1080Ti,
             DeviceCodeGtx1080,
+            DeviceCodeGtx1070Ti,
             DeviceCodeGtx1070,
             DeviceCodeGtx1060With6G,
             DeviceCodeGtx1060With3G,
